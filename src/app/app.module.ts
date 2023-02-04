effc3b152129d1c5559ea3d88c90a5f0e18339ca
saikrishnaBurra02 <saikrishna.burra04@gmail.com>
2023-02-04 11:58:14 -0500

    intial commit for the project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './pages/home/components/product-list/product-list.component';
+import { CartListComponent } from './pages/cart/components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule,
