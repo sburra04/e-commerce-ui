2fc1c5d5b26b97f2fecd505d6e4df3c587a7210a
saikrishnaBurra02 <saikrishna.burra04@gmail.com>
2023-02-05 09:46:16 -0500

    Data types added

---
+++ README.md
@@ -0,0 +1,15 @@
+Data Types:
+
+<!-- Number --> studentNumber:number = 106;
+<!-- string   --> studentName:string = 'sai'
+<!-- boolean --> IsStudentAvailable:boolean = false ;
+<!-- array --> studentsList:any = [1, 12, 38];
+<!-- array number --> studentList:number[] = [1.32.786];
+<!-- array string -->studentsNames:string[] = ['sai','ayush'];
+<!-- object --> students:any = {
+    studentNumber:1892,
+    studentname:'Sai',
+    studentEmailId:'saiburra761@gmail.com',
+    IsStudentAvailable:true,
+    studentCountries:['India','USA','Canada']
+    }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './pages/home/components/product-list/product-list.component';
 import { CartListComponent } from './pages/cart/components/cart-list/cart-list.component';
+import { LoginFormComponent } from './pages/login/components/login-form/login-form.component';
+import { SignupFormComponent } from './pages/signup/components/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    CartListComponent
+    CartListComponent,
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
